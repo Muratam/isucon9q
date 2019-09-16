c6bdc85b280aba449dd4ce7bca86e194087dc3a2
aokabi <aokabi@gmail.com>
2019-09-16 19:00:45 +0900

    deadlock

--- postapi.go
+++ postapi.go
@@ -283,7 +283,7 @@ func postBuy(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Print(err)
 
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
+		outputErrorMsg(w, http.StatusForbidden, "item is not for sale")
 		tx.Rollback()
 		return
 	}
